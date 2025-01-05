011a9611018169af72785ba2aae3c600b1be8bf3
Rechtorik <rechtorik6@stud.uniza.sk>
2025-01-06 00:44:22 +0100

    pridanie pomaleho vypisu a ukoncenia pomocou k, refaktoring menu

--- common/vykreslenieStruct.h
+++ common/vykreslenieStruct.h
@@ -5,6 +5,11 @@ typedef struct Vykreslenie_shm{
   Mapa mapa;
   int end;
   int pripojenie;
+  int pocetPripojenych;
+  int replikacia;
+  int pocetReplikacii;
+  int zacX;
+  int zacY;
 } Vykreslenie_shm;
 
 #endif // PREKAZKY
